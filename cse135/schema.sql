024b9eff9b9f3975b0f39ec8fc0311665c2cb56c
Alan Luong <aluong91@yahoo.com>
2013-05-12 16:58:30 -0700

    added db schema

---
+++ cse135/schema.sql
@@ -0,0 +1,20 @@
+﻿create table users (
+	ID serial primary key,
+	name text unique not null,
+	role text,
+	age integer,
+	state text
+);
+
+create table categories (
+	ID serial primary key,
+	name text unique not null,
+	description text
+);
+
+create table products (
+	ID serial primary key,
+	name text not null,
+	sku text unique not null,
+	category integer references categories(ID)
+);
