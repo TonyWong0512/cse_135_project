d7c5dd69eebd4fb6358d5057b0f3989653312bbf
Alan Luong <aluong91@yahoo.com>
2013-05-13 02:15:34 -0700

    modified insert query

--- cse135/src/Product.java
+++ cse135/src/Product.java
@@ -1,10 +1,17 @@
 
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import util.DbUtil;
 
 /**
  * Servlet implementation class Product
@@ -31,7 +38,22 @@ public class Product extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String sku = request.getParameter("sku");
+		Double price = Double.parseDouble(request.getParameter("price"));
+		String category = request.getParameter("category");
+		Connection conn = DbUtil.getConnection();
+		try {
+			PreparedStatement ps = conn.prepareStatement("insert into products(name, sku, price, category) values (?, ?, ?, ?)");
+			ps.setString(1, name);
+			ps.setString(2, sku);
+			ps.setBigDecimal(3, new BigDecimal(price));
+			ps.setString(4, category);
+			ps.executeUpdate();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
