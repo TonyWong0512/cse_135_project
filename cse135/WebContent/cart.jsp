7874c80cbd56d83a4fb26d6b4c516c93ff20a602
Cesar <cesarvegaro@gmail.com>
2013-05-15 15:26:44 -0700

    Adds shopping cart functionality. Has to show quantity

--- cse135/WebContent/cart.jsp
+++ cse135/WebContent/cart.jsp
@@ -2,67 +2,52 @@
 <h1>Products</h1>
 
 <div class="container-fluid" style="width: 60%">
-	<h1>Do you really want to add this product to your Shopping Cart?</h1>
-	<table class="table">
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Quantity</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<form action="/cse135/order" method="GET">
-					<td>Product 1</td>
-					<td><input type="number" style="width: 45px; padding: 1px"
-						value="0"></td>
-					<td><input type="submit" value="Add"></td>
-				</form>
-			</tr>
-		</tbody>
-	</table>
+	<c:if test="${product_to_buy != null}">
+		<h1>Do you really want to add this product to your Shopping Cart?</h1>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Quantity</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<form action="/cse135/order" method="GET">
+						<input type="hidden" name="id" value="<c:out value="${product_to_buy.id}" />">
+						<input type="hidden" name="action" value="add_to_cart" />
+						<td><c:out value="${product_to_buy.name}" /></td>
+						<td><input type="number" style="width: 45px; padding: 1px"
+							value="0"></td>
+						<td><input type="submit" value="Add"></td>
+					</form>
+				</tr>
+			</tbody>
+		</table>
+	</c:if>
 
-	<table class="table">
-		<thead>
-			<tr>
-				<th>
-					<h3>Your Shopping Cart</h3>
-				</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>Product 1</td>
-				<td>Small</td>
-				<td><input type="number" style="width: 45px; padding: 1px"
-					value="0"></td>
-				<td>Medium</td>
-				<td><input type="number" style="width: 45px; padding: 1px"
-					value="0"></td>
-				<td>Large</td>
-				<td><input type="number" style="width: 45px; padding: 1px"
-					value="0"></td>
-				<td>Ultra</td>
-				<td><input type="number" style="width: 45px; padding: 1px"
-					value="0"></td>
-			</tr>
-			<tr>
-				<td>Product 2</td>
-				<td>Small</td>
-				<td><input type="number" style="width: 45px; padding: 1px"
-					value="0"></td>
-				<td>Medium</td>
-				<td><input type="number" style="width: 45px; padding: 1px"
-					value="0"></td>
-				<td>Large</td>
-				<td><input type="number" style="width: 45px; padding: 1px"
-					value="0"></td>
-				<td>Ultra</td>
-				<td><input type="number" style="width: 45px; padding: 1px"
-					value="0"></td>
-			</tr>
-		</tbody>
-	</table>
 
+	<h3>Your Shopping Cart</h3>
+	<c:if test="${cart != null}">
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Quantity</th>
+				</tr>
+			</thead>
+			<tbody>
+			<c:forEach items="${cart}" var="product">
+				<tr>
+					<td><c:out value="${product.name}" /></td>
+					<td><c:out value="${product.id}" /></td>				
+				</tr>
+			</tbody>
+			</c:forEach>			
+		</table>
+	</c:if>
+	<c:if test="${cart == null}">
+		<h4>Your cart is empty</h4>
+	</c:if>
 </div>
 <%@include file="footer.jsp"%>
--- cse135/src/controller/ShoppingController.java
+++ cse135/src/controller/ShoppingController.java
@@ -1,7 +1,7 @@
 package controller;
 
-
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import model.Product;
 import dao.ProductDao;
@@ -19,31 +20,68 @@ import dao.ProductDao;
 public class ShoppingController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static String SHOPPING_CART = "/cart.jsp";
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ShoppingController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ShoppingController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		ProductDao dao = new ProductDao();
-		
-		List<Product> product = dao.getProduct(Integer.parseInt(request.getParameter("id")));
-		request.setAttribute("products", product);
-		
+		HttpSession session = request.getSession();
+		// Get old cart
+		List<Product> cart = (List<Product>) session.getAttribute("shopping_cart");
+		if (cart == null) {
+			cart = new ArrayList<Product>();
+		}
+		String action = request.getParameter("action");
+		// Add to the cart
+		if (action != null && action.equals("add_to_cart")) {
+			try {
+				int id_product_buying = Integer.parseInt(request
+						.getParameter("id"));
+				// Get the product
+				Product product_buying = dao.getProduct(id_product_buying).get(
+						0);
+				// Add it to the cart
+				cart.add(product_buying);
+				session.setAttribute("shopping_cart", cart);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+		}
+		// Ask if want to add to cart
+		else if (action != null && action.equals("add")) {
+			try {
+				int id_product_to_buy = Integer.parseInt(request
+						.getParameter("id"));
+				List<Product> product_to_buy = dao
+						.getProduct(id_product_to_buy);
+				request.setAttribute("product_to_buy", product_to_buy.get(0));
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+		}
+		request.setAttribute("cart", cart);
+
 		RequestDispatcher view = request.getRequestDispatcher(SHOPPING_CART);
 		view.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
