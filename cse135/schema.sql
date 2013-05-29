a1b1a81ef3f08299418f5ec58fd98fa9fcdef71f
Alan Luong <aluong91@gmail.com>
2013-05-29 03:42:10 -0700

    started triggers

--- cse135/schema.sql
+++ cse135/schema.sql
@@ -1,6 +1,9 @@
-DROP TYPE IF EXISTS season;
+DROP TYPE IF EXISTS season CASCADE;
 DROP TABLE IF EXISTS sales_by_customer;
 DROP TABLE IF EXISTS sales_by_state;
+DROP TRIGGER IF EXISTS update_sales on sales;
+DROP TRIGGER IF EXISTS add_sales on sales;
+DROP TRIGGER IF EXISTS remove_sales on sales;
 
 CREATE TYPE season AS ENUM ('summer', 'fall', 'winter', 'spring');
 
@@ -15,3 +18,10 @@ CREATE TABLE sales_by_state (
 	season season NOT NULL,
 	sales bigint NOT NULL
 );
+
+CREATE OR REPLACE FUNCTION add_sales() RETURNS TRIGGER AS '
+BEGIN
+	INSERT INTO sales_by_state DEFAULT VALUES;
+END' LANGUAGE 'plpgsql';
+
+CREATE TRIGGER add_sales AFTER INSERT ON sales EXECUTE PROCEDURE add_sales()
