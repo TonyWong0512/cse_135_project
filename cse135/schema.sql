9635d2f1f8db769a644b42dfc1a9555a9ffe9772
Alan Luong <aluong91@gmail.com>
2013-05-29 09:55:29 -0700

    added season_of function

--- cse135/schema.sql
+++ cse135/schema.sql
@@ -8,20 +8,31 @@ DROP TRIGGER IF EXISTS remove_sales on sales;
 CREATE TYPE season AS ENUM ('summer', 'fall', 'winter', 'spring');
 
 CREATE TABLE sales_by_customer (
-	customer int REFERENCES customers(id) NOT NULL,
+	customer int REFERENCES customers(id) UNIQUE NOT NULL,
 	season season NOT NULL,
 	sales bigint NOT NULL
 );
 
 CREATE TABLE sales_by_state (
-	state character(2) NOT NULL,
+	state character(2) UNIQUE NOT NULL,
 	season season NOT NULL,
 	sales bigint NOT NULL
 );
-
+/*
 CREATE OR REPLACE FUNCTION add_sales() RETURNS TRIGGER AS '
 BEGIN
-	INSERT INTO sales_by_state DEFAULT VALUES;
+	UPDATE sales_by_state SET 
+	INSERT INTO sales_by_state VALUES (NEW.state, `fall`, NEW.total_price);
 END' LANGUAGE 'plpgsql';
+*/
 
-CREATE TRIGGER add_sales AFTER INSERT ON sales EXECUTE PROCEDURE add_sales()
+CREATE OR REPLACE FUNCTION season_of(month int) RETURNS season AS $$
+BEGIN
+	RETURN CASE
+		WHEN month >= 3 AND month <= 5 THEN 'spring'
+		WHEN month >= 6 AND month <= 8 THEN 'summer'
+		WHEN month >= 9 AND month <= 11 THEN 'fall'
+		ELSE 'winter' END;
+END$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER add_sales AFTER INSERT ON sales EXECUTE PROCEDURE add_sales();
