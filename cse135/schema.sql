eb073bbabd0222fafbc19f18ade0d27af57094fc
Alan Luong <aluong91@gmail.com>
2013-05-29 02:40:17 -0700

    created aggregate tables

--- cse135/schema.sql
+++ cse135/schema.sql
@@ -1,34 +1,17 @@
-DROP TABLE users CASCADE;
-DROP TABLE categories CASCADE;
-DROP TABLE products CASCADE;
-DROP TABLE sales CASCADE;
+DROP TYPE IF EXISTS season;
+DROP TABLE IF EXISTS sales_by_customer;
+DROP TABLE IF EXISTS sales_by_state;
 
-create table users (
-	id serial primary key,
-	name text unique not null,
-	role text,
-	age smallint,
-	state character(2)
+CREATE TYPE season AS ENUM ('summer', 'fall', 'winter', 'spring');
+
+CREATE TABLE sales_by_customer (
+	customer int REFERENCES customers(id) NOT NULL,
+	season season NOT NULL,
+	sales bigint NOT NULL
 );
 
-create table categories (
-	id serial primary key,
-	name text unique not null,
-	description text
-);
-
-create table products (
-	sku serial primary key,
-	name text unique not null,
-	price decimal(10,4) not null,
-	category_id integer references categories(id)
-);
-
-create table sales (
-	id serial primary key, 
-	product_id integer references products (sku) not null, 
-	customer_id integer references users (id) not null, 
-	quarter integer not null,
-	quantity integer not null,
-	total_cost integer not null,
+CREATE TABLE sales_by_state (
+	state character(2) NOT NULL,
+	season season NOT NULL,
+	sales bigint NOT NULL
 );
