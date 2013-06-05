808b671e8e94d0bc31ea16b3277a41580d7615b8
Alan Luong <aluong91@gmail.com>
2013-06-05 13:10:51 -0700

    started DashboardController

---
+++ cse135/src/controller/DashboardController.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class DashboardController {
+	private final static String[] STATES = { "AL", "AK", "AZ", "AR", "CA",
+			"CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
+			"KY", "LA", "ME", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT",
+			"NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR",
+			"PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
+			"WI", "WY" };
+}
