2a902e0e3ba97b00a567670ea3bc0129d18c7cd4
Alan Luong <aluong91@gmail.com>
2013-06-05 13:14:45 -0700

    servlet boilerplate

--- cse135/src/controller/DashboardController.java
+++ cse135/src/controller/DashboardController.java
@@ -1,10 +1,31 @@
 package controller;
 
-public class DashboardController {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DashboardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	private final static String[] STATES = { "AL", "AK", "AZ", "AR", "CA",
 			"CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
 			"KY", "LA", "ME", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT",
 			"NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR",
 			"PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
 			"WI", "WY" };
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("states", STATES);
+		RequestDispatcher view = request.getRequestDispatcher("/dashboard.jsp");
+		view.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
